77dd8dfd10798030e1e5f1765d0f7fc6264cde24
Luckymahanty <luckymahanty06@gmail.com>
2026-05-28 17:31:29 +0530

    Updated project files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello jenkins pipeline");
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+   res.write("Hello from Jenkins Auto Deployment");
+
+   res.end();
+
+});
+
+server.listen(3000, '0.0.0.0', () => {
+
+   console.log("Server running on port 3000");
+
+});
