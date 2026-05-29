9fa7a291d5b5d5c23aedd493771c4463de779b42
Luckymahanty <luckymahanty06@gmail.com>
2026-05-29 16:51:57 +0530

    checking

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-   res.write("Hello from Jenkins Auto Deployment2");
+   res.write("Hello from Jenkins Auto Deployment2   n");
 
    res.end();
 
